848fb60217e032227ca96ddc1cb2ec5f626e0bdb
Sakshi Tiwari <tiwarishakshi318@gmail.com>
2025-11-27 22:00:32 +0530

    Update resume_parser.py

--- utils/resume_parser.py
+++ utils/resume_parser.py
@@ -1,1 +1,11 @@
+from langchain_community.document_loaders import PyPDFLoader
+import os
 
+def extract_text_from_pdf(file):
+    path = f"./temp_{file.name}"
+    with open(path, "wb") as f:
+        f.write(file.getvalue())
+    loader = PyPDFLoader(path)
+    pages = loader.load()
+    os.remove(path)
+    return "\n".join([page.page_content for page in pages])
