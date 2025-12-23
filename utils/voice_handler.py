448116723283ffb527ee0da5e6951b60b60fcf61
Sakshi Tiwari <tiwarishakshi318@gmail.com>
2025-12-23 20:28:51 +0530

    Delete utils directory

--- utils/voice_handler.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def voice_input():
-    st.write("Voice mode अभी active है – बोलो:")
-    # हम बाद में full voice डाल देंगे, अभी placeholder
-    return st.text_input("या यहाँ टाइप करके test करो:")
